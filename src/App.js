d5b6cbf8772dde17c8461d2809aaa117ba63ccd9
unknown <pintoydc@gmail.com>
2022-01-31 19:51:07 +0200

    Person component and App component are populated with new data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Car from './components/Car';
 import NewCar from './components/NewCar/NewCar';
+import Person from './components/Person';
+
 
 function App() {
 
@@ -28,6 +30,55 @@ function App() {
   ];
 
 
+
+  const personDetail = [
+    {
+      id: 0,
+      name: 'Pinto',
+      surname: 'Manuel',
+      age: 34,
+      nationality: 'Angolan',
+      profession: 'Frontend Developer'
+    },
+
+    {
+      id: 1,
+      name: 'Adriano',
+      surname: 'Manuel',
+      age: 34,
+      nationality: 'Angolan',
+      profession: 'Petroleun engineer'
+    },
+
+    {
+      id: 2,
+      name: 'Constancia',
+      surname: 'Manuel',
+      age: 34,
+      nationality: 'Angolan',
+      profession: 'Teacher'
+    },
+
+    {
+      id: 3,
+      name: 'Madalena',
+      surname: 'Manuel',
+      age: 34,
+      nationality: 'Angolan',
+      profession: 'Student'
+    },
+
+    {
+      id: 4,
+      name: 'Tunecas',
+      surname: 'Manuel',
+      age: 34,
+      nationality: 'Angolan',
+      profession: 'Student'
+    }
+  ]
+
+
   const addCarHandler = Car => {
     console.log('In app');
     console.log(Car)
@@ -39,7 +90,53 @@ function App() {
     <div className="App">
 
         <NewCar onAddcar={addCarHandler} />
-        
+
+
+
+        <Person
+            name = {personDetail[0].name}
+            surname = {personDetail[0].surname}
+            age = {personDetail[0].age}
+            nationality = {personDetail[0].nationality}
+            profession = {personDetail[0].profession}
+        />
+
+        <Person
+            name = {personDetail[1].name}
+            surname = {personDetail[1].surname}
+            age = {personDetail[1].age}
+            nationality = {personDetail[1].nationality}
+            profession = {personDetail[1].profession}
+        />
+
+        <Person
+            name = {personDetail[2].name}
+            surname = {personDetail[2].surname}
+            age = {personDetail[2].age}
+            nationality = {personDetail[2].nationality}
+            profession = {personDetail[2].profession}
+        />
+
+        <Person
+            name = {personDetail[3].name}
+            surname = {personDetail[3].surname}
+            age = {personDetail[3].age}
+            nationality = {personDetail[3].nationality}
+            profession = {personDetail[3].profession}
+        />
+
+       <Person
+            name = {personDetail[4].name}
+            surname = {personDetail[4].surname}
+            age = {personDetail[4].age}
+            nationality = {personDetail[4].nationality}
+            profession = {personDetail[4].profession}
+        />
+
+
+
+
+        {/**Displaying the car array **/}
         <Car 
            name = {carDetail[0].name}
            maker = {carDetail[0].maker}
