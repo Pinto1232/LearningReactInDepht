eb561859a6c947fc8d22b05b0f4104da11377b19
Pinto Manuel <pintoydc@gmail.com>
2022-02-04 09:05:40 +0200

    Array created and data passed thru props

--- src/App.js
+++ src/App.js
@@ -2,11 +2,49 @@ import './App.css';
 import ExpensesItem from './components/ExpensesItem';
 
 
+const ExpenseDetail = [ 
+  {
+    id: 0,
+    title: 'Water',
+    price: 'R1200',
+    date: new Date(2022, 7, 14)
+  },
+  {
+    id: 1,
+    title: 'Food',
+    price: 'R3200',
+    date: new Date(2022, 7, 14)
+  },
+  {
+    id: 2,
+    title: 'Diesel',
+    price: 'R1600',
+    date: new Date(2022, 7, 14)
+  }
+]
+
+
 function App() {
 
   return (
     <div className="App">
-      <ExpensesItem />
+      <ExpensesItem 
+         title = {ExpenseDetail[0].title}
+         price = {ExpenseDetail[0].price}
+         date = {ExpenseDetail[0].date}
+      />
+
+      <ExpensesItem 
+         title = {ExpenseDetail[1].title}
+         price = {ExpenseDetail[1].price}
+         date = {ExpenseDetail[1].date}
+      />
+
+      <ExpensesItem 
+         title = {ExpenseDetail[2].title}
+         price = {ExpenseDetail[2].price}
+         date = {ExpenseDetail[2].date}
+      />
     </div>
   );
 }
--- src/components/ExpensesItem.js
+++ src/components/ExpensesItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './ExpensesItem.css'
 
-const ExpensesItem = () => {
+const ExpensesItem = (props) => {
     return (
-        <div>
-            <div>Date</div>
-            <div>
-                <h2>Title</h2>
-                <div>Amount</div>
+        <div className='expense-item'>
+            <div className='expense-item__date'>{props.date.toISOString()}</div>
+            <div className='expense-item__description'>
+                <h2 className=''>{props.title}</h2>
+                <div className='expense-item__price'>{props.price}</div>
+                <button>Click me</button>
             </div>
         </div>
     );
