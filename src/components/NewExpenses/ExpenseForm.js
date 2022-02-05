1f238fae9ff2a18d0efe34168b822a16aa5b68f8
Pinto Manuel <pintoydc@gmail.com>
2022-02-05 16:29:38 +0200

    Getting the value typed on the field and store it

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -4,36 +4,46 @@ import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
-    const addInputHandler = (event) => {
-             event.preventDefault()
+
+    const titleChangeHandler = (event) => {
+           console.log(event.target.value)
+    }
+
+    const amountChangeHandler = (event) => {
+            console.log(event.target.value)
+    }
+
+    const dateChangeHandler = () => {
+
     }
 
 
 
 
+
     return (
-            <form onChange={addInputHandler}>
+            <form>
                 <h2>Expenform Data Capture</h2>
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
-                        <input type="text" />
+                        <input type="text" 
+                         onChange={titleChangeHandler} 
+
+                         />
                     </div>
                 </div>
 
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Amount</label>
-                        <input type="text"/>
+                        <input type="text"
+                           onChange={amountChangeHandler}
+
+                         />
                     </div>
                 </div>
 
-                <div className='new-expense__controls'>
-                    <div className='new-expense__control'>
-                        <label>Amount</label>
-                        <input type="number" min='0.01' step='0.01' />
-                    </div>
-                </div>
 
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
