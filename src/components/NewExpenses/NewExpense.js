85cb0748bf038c28bbc23a19216e6184f367e322
Pinto Manuel <pintoydc@gmail.com>
2022-02-05 19:08:02 +0200

    function is created and map it into the ExpenseForm component in the NewExpense component

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -3,9 +3,24 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 
 const NewExpense = () => {
+    /* Creating a function to pass in the onSaveExpenseData function */
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+
+        /* Passing the expenseData that exist into the ExpenseForm submitHandler function */
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+
+        };
+    };
+
+
     return (
         <div className='new-expense'>
-           <ExpenseForm />
+            {/* Passing a function inside of the ExpenseForm component
+            to be called in the app parent component */}
+
+           <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
         </div>
     );
 };
