f1e8a7cb77b3eace6feb3ca6ac98bd08cb1cff89
Pinto Manuel <pintoydc@gmail.com>
2022-02-06 13:24:54 +0200

    Latest comment on this branch

--- src/components/NewExercise/NewExercise.js
+++ src/components/NewExercise/NewExercise.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ExerciseForm from './ExerciseForm';
 import './NewExercise.css'
 
-const NewExercise = () => {
+const NewExercise = (props) => {
+
+    const saveExerciseHandler = (enteredExerciseData) => {
+        const exerciseData = {
+            ...enteredExerciseData,
+            id: Math.random().toString()
+
+        };
+        props.onAddExercise(exerciseData);
+        
+        
+
+    }
     return (
         <div className='new-exercise'>
-            <ExerciseForm />
+            <ExerciseForm onSaveExerciseData = {saveExerciseHandler}/>
         </div>
     );
 };
