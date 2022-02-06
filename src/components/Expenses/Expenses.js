625c5a45ea99c85a236355fc85f2f072e32a4157
Pinto Manuel <pintoydc@gmail.com>
2022-02-06 16:57:55 +0200

    Nothing more then a simple commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -24,7 +24,7 @@ const Expenses = (props) => {
            />
         )}
 
-      {/*   <ExpenseItem
+      {/*<ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
