9fd97563449f16ded80b13b932e7bb78819b1899
Pinto Manuel <pintoydc@gmail.com>
2022-02-18 08:52:36 +0200

    Chart is fully working

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -3,13 +3,17 @@ import ChartBar from './ChartBar';
 import './Charts.css'
 
 const Charts = (props) => {
+    const dataPointValue = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValue)
+
+
     return (
         <div className='chart'>
             {props.dataPoints.map(dataPoint => 
                <ChartBar 
                  key={dataPoint.label}
                  value={dataPoint.value}
-                 maxValue={null}
+                 maxValue={totalMaximum}
                  label={dataPoint.label}
             />)}
         </div>
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -4,6 +4,7 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -40,6 +41,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
 
+        <ExpensesChart expenses={filteredExpenses}/>
 
         {/* Outputing conditional content */}
         
